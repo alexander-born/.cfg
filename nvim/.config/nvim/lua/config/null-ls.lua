9148124de69dca0c5122ac090c3b4f619539ff6e
Alexander Born <alexander.born@bmw.de>
2022-03-17 15:15:30 +0100

    Add json formatter

--- nvim/.config/nvim/lua/config/null-ls.lua
+++ nvim/.config/nvim/lua/config/null-ls.lua
@@ -6,6 +6,7 @@ function M.setup()
             require("null-ls").builtins.formatting.black,
             require("null-ls").builtins.formatting.buildifier,
             require("null-ls").builtins.formatting.clang_format,
+            require("null-ls").builtins.formatting.json_tool,
         },
         on_attach = function(client)
             if client.resolved_capabilities.document_formatting then
