c008e394a4f6374ad188bb9509c21df07d4c27a0
Alexander Born <alexander.born@bmw.de>
2022-05-07 14:17:57 +0200

    Make nvim config prettier, lualine, lsp signs, diagnostics

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -54,6 +54,12 @@ function M.setup()
         require('lspconfig')[server].setup(config)
     end
 
+    local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl })
+    end
+
 end
 
 return M
--- nvim/.config/nvim/lua/config/lualine.lua
+++ nvim/.config/nvim/lua/config/lualine.lua
@@ -30,17 +30,36 @@ local function get_git_branch()
     return ' ' .. head
 end
 
-local colors = get_colors()
+local function lsp_server()
+    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+    local clients = vim.lsp.get_active_clients()
+    for _, client in ipairs(clients) do
+      local filetypes = client.config.filetypes
+      if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 and client.name ~= 'null-ls' then
+        return client.name
+      end
+    end
+    return ''
+end
+
 
 local M = {}
 function M.setup()
+  local colors = get_colors()
+  local theme = vim.g.color_scheme
+  if vim.g.color_scheme == "everforest" then
+      theme = require'lualine.themes.everforest'
+      local normal = theme.normal.a
+      theme.normal.a = theme.insert.a
+      theme.insert.a = normal
+  end
   require'lualine'.setup {
-    options = { theme = vim.g.color_scheme, globalstatus = true },
+    options = { theme = theme, globalstatus = true },
     sections = {
       lualine_a = {'mode'},
       lualine_b = {{get_git_branch}},
       lualine_c = {{get_filename}, { 'diff', diff_color = { added = { fg = colors.green }, modified = { fg = colors.orange }, removed = { fg = colors.red }}, symbols = {added = ' ', modified = ' ', removed = ' '} }},
-      lualine_x = {{'diagnostics', sources = {'nvim_diagnostic'}, symbols = {error = ' ', warn = ' ', info = ' '}}, {diagnostics_ok}, 'filetype' }, 
+      lualine_x = {{'diagnostics', sources = {'nvim_diagnostic'}, symbols = {error = ' ', warn = ' ', info = ' '}, diagnostics_color = {hint = {fg = colors.green}}}, {diagnostics_ok}, {lsp_server}, 'filetype' },
       lualine_y = {'progress'},
       lualine_z = {'location'}
     },
