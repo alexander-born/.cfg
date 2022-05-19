ad921feaaa598b212bdcf83af512300e4be07006
Alexander Born <alexander.born@bmw.de>
2022-05-19 07:55:46 +0200

    UpdateConfig to also stow

--- nvim/.config/nvim/plugin/user_commands.lua
+++ nvim/.config/nvim/plugin/user_commands.lua
@@ -1,6 +1,8 @@
 function UpdateConfig()
     vim.cmd('!git -C ~/.cfg pull')
     vim.cmd('!git -C ~/.cfg_work pull')
+    vim.cmd('!stow nvim -d ~/.cfg -t ~')
+    vim.cmd('!stow nvim -d ~/.cfg_work -t ~')
 end
 
 vim.cmd[[command! UpdateConfig execute "lua UpdateConfig()"]]
