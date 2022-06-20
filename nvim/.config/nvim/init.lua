799791f17e4cb5addfe568cfca01525ca200fbfd
Alexander Born <alexander.born@bmw.de>
2022-06-20 09:04:19 +0200

    Update bash

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,3 +6,4 @@ require'color'
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', { callback = function() vim.highlight.on_yank() end, group = highlight_group, pattern = '*', })
 
+pcall(require, 'user')
