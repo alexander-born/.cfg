a0554b4332cb7daaf0c8a6ccabb179a698ee107d
Alexander Born <alexander.born@bmw.de>
2022-08-25 11:56:51 +0200

    Add PyrightForBazelSetup command for til

--- nvim/.config/nvim/lua/utils.lua
+++ nvim/.config/nvim/lua/utils.lua
@@ -61,3 +61,4 @@ end
 
 vim.cmd[[command! UpdateConfig execute "lua UpdateConfig()"]]
 vim.cmd[[command! SetupPyrightForBazel execute "lua require'config.lsp'.setup_pyright_with_bazel()"]]
+vim.cmd[[command! PyrightForBazelSetup execute "lua require'config.lsp'.setup_pyright_with_bazel()"]]
