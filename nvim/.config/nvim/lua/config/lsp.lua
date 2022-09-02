815d7c29d420b471660103b06c6b31192c3663e0
Alexander Born <alexander.born@bmw.de>
2022-09-02 15:57:30 +0200

    Use active python venv or Pipfile for pyright

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -1,3 +1,23 @@
+local util = require('lspconfig/util')
+local path = util.path
+
+local function get_python_path(workspace)
+  -- Use activated virtualenv.
+  if vim.env.VIRTUAL_ENV then
+    return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
+  end
+
+  -- Find and use virtualenv from pipenv in workspace directory.
+  local match = vim.fn.glob(path.join(workspace, 'Pipfile'))
+  if match ~= '' then
+    local venv = vim.fn.trim(vim.fn.system('PIPENV_PIPFILE=' .. match .. ' pipenv --venv'))
+    return path.join(venv, 'bin', 'python')
+  end
+
+  -- Fallback to system Python.
+  return vim.fn.exepath('python3') or vim.fn.exepath('python') or 'python'
+end
+
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
@@ -45,6 +65,9 @@ function M.setup()
         local config = { capabilities = capabilities }
         if server == "sumneko_lua" then
           config.settings = lua_settings
+        end
+        if server == "pyright" then
+            config.on_init = function(client) client.config.settings.python.pythonPath = get_python_path(client.config.root_dir) end
         end
         if server == "clangd" then
             config.cmd = {require'mason-core.path'.bin_prefix('clangd'), "--background-index" }
