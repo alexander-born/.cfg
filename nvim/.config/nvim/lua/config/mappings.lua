d42789a93650758825ec5c2b4f54fa9d0ab44c09
Alexander Born <alexander.born@bmw.de>
2022-09-09 10:48:00 +0200

    Update to bazel plugin

--- nvim/.config/nvim/lua/config/mappings.lua
+++ nvim/.config/nvim/lua/config/mappings.lua
@@ -121,13 +121,13 @@ function M.setup()
     vim.api.nvim_create_autocmd("FileType", { pattern = "bzl", callback = function() map('n', 'gd', vim.fn.GoToBazelDefinition, { buffer = 0, desc = "Goto Definition" }) end })
     vim.api.nvim_create_autocmd("FileType", { pattern = "bzl", callback = function() map('n', '<Leader>y',   my_bazel.YankLabel, {desc = "Bazel Yank Label"}) end })
     map('n', 'gbt',         vim.fn.GoToBazelTarget, {desc = "Goto Bazel Build File"})
-    map('n', '<Leader>bl',  bazel.run_bazel, {desc = "Bazel Last"})
+    map('n', '<Leader>bl',  bazel.run_last, {desc = "Bazel Last"})
     map('n', '<Leader>bdt', my_bazel.DebugTest, {desc = "Bazel Debug Test"})
     map('n', '<Leader>bdr', my_bazel.DebugRun, {desc = "Bazel Debug Run"})
-    map('n', '<Leader>bt',  function() bazel.run_bazel_here("test "  .. vim.g.bazel_config) end, {desc = "Bazel Test"})
-    map('n', '<Leader>bb',  function() bazel.run_bazel_here("build " .. vim.g.bazel_config) end, {desc = "Bazel Build"})
-    map('n', '<Leader>br',  function() bazel.run_bazel_here("run "   .. vim.g.bazel_config) end, {desc = "Bazel Run"})
-    map('n', '<Leader>bdb', function() bazel.run_bazel_here("build " .. vim.g.bazel_config .. " -c dbg --cxxopt=-O0 ") end, {desc = "Bazel Debug Build"})
+    map('n', '<Leader>bt',  function() bazel.run_here("test "  .. vim.g.bazel_config) end, {desc = "Bazel Test"})
+    map('n', '<Leader>bb',  function() bazel.run_here("build " .. vim.g.bazel_config) end, {desc = "Bazel Build"})
+    map('n', '<Leader>br',  function() bazel.run_here("run "   .. vim.g.bazel_config) end, {desc = "Bazel Run"})
+    map('n', '<Leader>bdb', function() bazel.run_here("build " .. vim.g.bazel_config .. " -c dbg --cxxopt=-O0 ") end, {desc = "Bazel Debug Build"})
 
     -- debugger
     local dap = require'telescope'.extensions.dap
