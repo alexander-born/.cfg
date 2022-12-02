0afcbba67413314e60da0413cef13c2ad29be896
Alexander Born <alexander.born@bmw.de>
2022-12-02 21:12:37 +0100

    Update telescope project

--- nvim/.config/nvim/lua/config/telescope.lua
+++ nvim/.config/nvim/lua/config/telescope.lua
@@ -1,14 +1,16 @@
 local M = {}
 
 function M.setup()
-    local project_actions = require("telescope._extensions.project.actions")
+    local project_actions = require'telescope._extensions.project.actions'
+    local telescope = require'telescope'
+    local actions = require'telescope.actions'
     local work_actions_available, work_project_actions = pcall(require, 'work.project.actions')
-    require'telescope'.setup({
+    telescope.setup({
         defaults = {
             mappings = {
                 i = {
-                    ["<C-j>"] = require('telescope.actions').cycle_history_next,
-                    ["<C-k>"] = require('telescope.actions').cycle_history_prev,
+                    ["<C-j>"] = actions.cycle_history_next,
+                    ["<C-k>"] = actions.cycle_history_prev,
                 },
             },
         },
@@ -20,16 +22,17 @@ function M.setup()
               case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
             },
             project = {
-              on_project_selected = function(prompt_bufnr)
-                project_actions.change_working_directory(prompt_bufnr, false)
-                if work_actions_available then work_project_actions(prompt_bufnr) end
-              end
+                base_dirs = {{path = '~', max_depth = 4},},
+                on_project_selected = function(prompt_bufnr)
+                  project_actions.change_working_directory(prompt_bufnr, false)
+                  if work_actions_available then work_project_actions(prompt_bufnr) end
+                end
             }
         }
     })
-    require'telescope'.load_extension('fzf')
-    require'telescope'.load_extension('dap')
-    require'telescope'.load_extension('project')
+    telescope.load_extension('fzf')
+    telescope.load_extension('dap')
+    telescope.load_extension('project')
 end
 
 return M
