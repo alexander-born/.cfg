e01c7825bf2e8586cc46a0ccbb9fa0320459a3a8
Alexander Born <alexander.born@bmw.de>
2023-01-10 14:38:17 +0100

    Update

--- nvim/.config/nvim/lua/config/dap.lua
+++ nvim/.config/nvim/lua/config/dap.lua
@@ -3,14 +3,14 @@ local M = {}
 local function split_by_space(input)
     local chunks = {}
     for substring in input:gmatch("%S+") do
-       table.insert(chunks, substring)
+        table.insert(chunks, substring)
     end
     return chunks
 end
 
 function M.set_python_args(args)
     vim.g.python_debug_args = split_by_space(args)
-    require'dap'.configurations.python[1].args = vim.g.python_debug_args
+    require 'dap'.configurations.python[1].args = vim.g.python_debug_args
 end
 
 function M.set_python_args_from_input()
@@ -19,13 +19,14 @@ function M.set_python_args_from_input()
 end
 
 function M.end_debug_session()
-    require'dap'.terminate()
-    require'dapui'.close()
+    require 'dap'.terminate()
+    require 'dapui'.close()
 end
 
 function M.setup()
-    vim.fn.sign_define('DapBreakpoint', {text='', texthl='DiagnosticSignError', linehl='', numhl=''})
-    vim.fn.sign_define('DapStopped', {text='', texthl='DiagnosticSignWarn', linehl='Visual', numhl='DiagnosticSignWarn'})
+    vim.fn.sign_define('DapBreakpoint', { text = '', texthl = 'DiagnosticSignError', linehl = '', numhl = '' })
+    vim.fn.sign_define('DapStopped', { text = '', texthl = 'DiagnosticSignWarn', linehl = 'Visual',
+        numhl = 'DiagnosticSignWarn' })
     require('dap-python').test_runner = 'pytest'
     require('dap.ext.vscode').load_launchjs()
 
@@ -35,9 +36,9 @@ function M.setup()
     dap.listeners.after.event_exited["dapui_config"] = function() dapui.close() end
 
     dap.adapters.cppdbg = {
-      id = 'cppdbg',
-      type = 'executable',
-      command = 'OpenDebugAD7',
+        id = 'cppdbg',
+        type = 'executable',
+        command = 'OpenDebugAD7',
     }
 
     -- dap.adapters.python = {
@@ -58,26 +59,26 @@ function M.setup()
     -- }
 
     dapui.setup({
-      layouts = {
-        {
-          elements = {
-            'scopes',
-            'breakpoints',
-            'watches',
-            'stacks',
-          },
-          size = 40,
-          position = 'left',
+        layouts = {
+            {
+                elements = {
+                    'scopes',
+                    'breakpoints',
+                    'watches',
+                    'stacks',
+                },
+                size = 40,
+                position = 'left',
+            },
+            {
+                elements = {
+                    'repl',
+                    'console',
+                },
+                size = 10,
+                position = 'bottom',
+            },
         },
-        {
-          elements = {
-            'repl',
-            'console',
-          },
-          size = 10,
-          position = 'bottom',
-        },
-      },
     })
 end
 
--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -1,66 +1,65 @@
-local util = require('lspconfig/util')
-local path = util.path
+local path = require 'lspconfig/util'.path
 
 local function get_python_path(workspace)
-  -- Use activated virtualenv.
-  if vim.env.VIRTUAL_ENV then
-    return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
-  end
-
-  -- Find and use virtualenv from pipenv in workspace directory.
-  local pipfile = path.join(workspace, 'Pipfile')
-  if vim.fn.filereadable(pipfile) == 1 then
-    local venv = vim.fn.trim(vim.fn.system('PIPENV_PIPFILE=' .. pipfile .. ' pipenv --venv'))
-    if vim.v.shell_error == 0 then
-        return path.join(venv, 'bin', 'python')
-    else
-        print("Virtual environment of Pipfile not yet created. To create: cd " .. workspace .. "; pipenv shell;")
+    -- Use activated virtualenv.
+    if vim.env.VIRTUAL_ENV then
+        return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
     end
-  end
 
-  -- Fallback to system Python.
-  return vim.fn.exepath('python3') or vim.fn.exepath('python') or 'python'
+    -- Find and use virtualenv from pipenv in workspace directory.
+    local pipfile = path.join(workspace, 'Pipfile')
+    if vim.fn.filereadable(pipfile) == 1 then
+        local venv = vim.fn.trim(vim.fn.system('PIPENV_PIPFILE=' .. pipfile .. ' pipenv --venv'))
+        if vim.v.shell_error == 0 then
+            return path.join(venv, 'bin', 'python')
+        else
+            print('Virtual environment of Pipfile not yet created. To create: cd ' .. workspace .. '; pipenv shell;')
+        end
+    end
+
+    -- Fallback to system Python.
+    return vim.fn.exepath('python3') or vim.fn.exepath('python') or 'python'
 end
 
 local M = {}
 
 function M.get_capabilities()
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-    capabilities.offsetEncoding = { "utf-16" }
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    capabilities.offsetEncoding = { 'utf-16' }
     return capabilities
 end
 
 function M.setup()
-    local servers = { "clangd", "pyright", "sumneko_lua", "bashls", "vimls" }
     require("neodev").setup()
-    require("mason").setup { providers = { "mason.providers.client", } }
-    require("mason-lspconfig").setup({ ensure_installed = servers, automatic_installation = true})
+    require("mason").setup({ providers = { 'mason.providers.client', } })
+
+    local lspconfig = require 'lspconfig'
+    local mason_lspconfig = require 'mason-lspconfig'
+    local bazel_root_dir = require 'config.bazel'.root_dir
+    local servers_to_install = { 'clangd', 'pyright', 'sumneko_lua', 'bashls' }
+    mason_lspconfig.setup { ensure_installed = servers_to_install, automatic_installation = true }
 
     local capabilities = M.get_capabilities()
 
-    for _, server in pairs(servers) do
+    for _, server in ipairs(mason_lspconfig.get_installed_servers()) do
         local config = { capabilities = capabilities }
-        if server == "pyright" then
+        if server == 'pyright' then
             config.on_init = function(client) client.config.settings.python.pythonPath = get_python_path(client.config.root_dir) end
-            config.root_dir = require'config.bazel'.root_dir(require'lspconfig.server_configurations.pyright'.default_config.root_dir)
+            config.root_dir = bazel_root_dir(require 'lspconfig.server_configurations.pyright'.default_config.root_dir)
         end
-        if server == "clangd" then
-            config.cmd = { "clangd", "--background-index", "--header-insertion=never", "--offset-encoding=utf-16" }
-            -- config.cmd = { "clangd", "--background-index", "--header-insertion=never", "--query-driver=**"  }
-            config.root_dir = require'config.bazel'.root_dir(require'lspconfig.server_configurations.clangd'.default_config.root_dir)
+        if server == 'clangd' then
+            config.cmd = { 'clangd', '--background-index', '--header-insertion=never', '--offset-encoding=utf-16' }
+            config.root_dir = bazel_root_dir(require 'lspconfig.server_configurations.clangd'.default_config.root_dir)
         end
-        require('lspconfig')[server].setup(config)
+        lspconfig[server].setup(config)
     end
 
-    local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+    local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
     for type, icon in pairs(signs) do
-      local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl })
+        local hl = 'DiagnosticSign' .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl })
     end
 
 end
 
 return M
-
-
