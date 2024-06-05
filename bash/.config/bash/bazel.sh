e30982253cfa844e3f7274ff8c1c61e0f3ac23ef
Alexander Born <alexander.born@bmw.de>
2024-06-05 15:04:32 +0200

    Update bazel refreshcompile commands

--- bash/.config/bash/bazel.sh
+++ bash/.config/bash/bazel.sh
@@ -1,57 +1,59 @@
 bazel_get_workspace_path() {
-  local workspace=$PWD
-  while true; do
-    if [ -f "${workspace}/WORKSPACE" ]; then
-      break
-    elif [ -z "$workspace" -o "$workspace" = "/" ]; then
-      workspace=$PWD
-      break;
-    fi
-    workspace=${workspace%/*}
-  done
-  echo $workspace
+	local workspace=$PWD
+	while true; do
+		if [ -f "${workspace}/WORKSPACE" ]; then
+			break
+		elif [ -z "$workspace" -o "$workspace" = "/" ]; then
+			workspace=$PWD
+			break
+		fi
+		workspace=${workspace%/*}
+	done
+	echo $workspace
 }
 
-
-add_hedron_to_workspace_file()
-{
-    echo '
+add_hedron_to_workspace_file() {
+	echo '
 local_repository(
     name = "hedron_compile_commands",
     path = "'$HOME'/projects/bazel-compile-commands-extractor",
 )
 load("@hedron_compile_commands//:workspace_setup.bzl", "hedron_compile_commands_setup")
-hedron_compile_commands_setup()' >> WORKSPACE
+hedron_compile_commands_setup()
+load("@hedron_compile_commands//:workspace_setup_transitive.bzl", "hedron_compile_commands_setup_transitive")
+hedron_compile_commands_setup_transitive()
+load("@hedron_compile_commands//:workspace_setup_transitive_transitive.bzl", "hedron_compile_commands_setup_transitive_transitive")
+hedron_compile_commands_setup_transitive_transitive()
+load("@hedron_compile_commands//:workspace_setup_transitive_transitive_transitive.bzl", "hedron_compile_commands_setup_transitive_transitive_transitive")
+hedron_compile_commands_setup_transitive_transitive_transitive()' >>WORKSPACE
 }
 
-add_refresh_compile_commands_target()
-{
-    echo '
+add_refresh_compile_commands_target() {
+	echo '
 load("@hedron_compile_commands//:refresh_compile_commands.bzl", "refresh_compile_commands")
 
 refresh_compile_commands(
     name = "refresh_compile_commands",
-    exclude_headers = "all",
-    exclude_external_sources = True,
-    targets = [' >> BUILD
-    echo '"'$1'",' >> BUILD
-    echo '],
-)' >> BUILD
+    # exclude_headers = "all",
+    exclude_external_sources = False,
+    targets = [' >>BUILD
+	echo '"'$1'",' >>BUILD
+	echo '],
+)' >>BUILD
 }
 
-refresh_compile_commands()
-{
-    git clone https://github.com/hedronvision/bazel-compile-commands-extractor.git ~/projects/bazel-compile-commands-extractor
-    git -C ~/projects/bazel-compile-commands-extractor pull
-    pushd $(bazel_get_workspace_path)
-    add_hedron_to_workspace_file
-    add_refresh_compile_commands_target $1
-    local config="${@:2}"
-    bazel run //:refresh_compile_commands -- $config --keep_going
-    git checkout WORKSPACE
-    git checkout BUILD
-    git checkout .gitignore
-    popd
+refresh_compile_commands() {
+	git clone https://github.com/hedronvision/bazel-compile-commands-extractor.git ~/projects/bazel-compile-commands-extractor
+	git -C ~/projects/bazel-compile-commands-extractor pull
+	pushd $(bazel_get_workspace_path)
+	add_hedron_to_workspace_file
+	add_refresh_compile_commands_target $1
+	local config="${@:2}"
+	bazel run //:refresh_compile_commands -- $config --keep_going
+	git checkout WORKSPACE
+	git checkout BUILD
+	git checkout .gitignore
+	popd
 }
 
 export -f add_hedron_to_workspace_file
